241b17424025f2b499d7e6fdc341e3bf1e1859b3
ValenAngeleri <vangeleri@mail.austral.edu.ar>
2025-04-03 16:16:44 -0300

    ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,13 @@
 def ada():
     first_name = "AdA"
-    last_name = "LoVeLAce"
+    last_name = "LoVeLAce"   
+    full_name =f"{first_name}{last_name}"
+    full_name2 =f"{first_name}{last_name}" 
+    full_name3 =f"{first_name} {last_name}"
+    print (full_name.lower())
+    print (full_name.title ())
+    print (full_name.upper())
+    print (f"\t{full_name.lower()}")
+
+ada ()
+
